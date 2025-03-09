3bcc78cfaa4cadca631d0026c0f668aec86348b9
guihp <g.henrique.barros21@gmail.com>
2025-03-09 09:42:36 -0300

    Adicionar configuração para Netlify e correções de roteamento

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
